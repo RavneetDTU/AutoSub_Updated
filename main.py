0b1ec11c0b3031148bb8547b7c0dba3fffe68f50
RavneetDTU <ravneet.dtu@gmail.com>
2022-11-04 02:09:38 +0530

    Added version 0.2

--- main.py
+++ main.py
@@ -1,105 +1,106 @@
-# 890wWM0lg94
-import datetime
-from pickle import APPEND
-import uvicorn
-from deepspeech import Model
-import numpy as np
 import os
-import wave
+import datetime
 import json
-import subprocess
-import shutil
-import scipy
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import JSONResponse
-from os.path import exists
 import srt
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
-from youtube_transcript_api import YouTubeTranscriptApi
-from youtube_transcript_api._errors import TranscriptsDisabled
-from fastapi import FastAPI, Depends, HTTPException,  Request
-from fastapi.responses import JSONResponse
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import PlainTextResponse
+from pytube import YouTube
+from deepspeech import Model
+import subprocess
+from fastapi import FastAPI, UploadFile, File
 import shutil
-from fastapi import FastAPI, File, UploadFile
-import uvicorn
-import time
-
-class UnicornException(Exception):
-    def __init__(self, name: str):
-        self.name = name
-
 
 app = FastAPI()
 
-@app.post("/files/" , tags=['rename the file to "video.mp4" before uploading'])
-async def create_file(file: UploadFile = File(...)):
-    if not file:
-        return {"message": "No file sent"}
-    else:
-        with open(f'{file.filename}',"wb") as buffer:
-            shutil.copyfileobj(file.file, buffer)
-        return {"filename": file.filename}
-
-
+output_base_path = './output_files/'
+youtube_audio_path = './youtube_audios/'
+audio_paths = 'youtube_audios/'
 
 model_file_path = 'deepspeech-0.9.3-models.pbmm'
 lm_file_path = 'deepspeech-0.9.3-models.scorer' 
 
+def download_audio(video_id:str):
+    url = "https://www.youtube.com/watch?v="+video_id
+    yt = YouTube(url)
+    video = yt.streams.filter(only_audio=True).first()
 
-list =[]
+    out_file = video.download(output_path=youtube_audio_path)
 
-@app.post("/deepspeechsrt/")
-async def main():
-    command = f"python3 autosub/main.py --model {model_file_path} --scorer {lm_file_path} --file video.mp4 --format srt"
-    
-    ret = subprocess.call(command, shell=True)
-    mysrt =""
-
-    with open("output/video.srt", 'r') as srtfile :
-        for line in srtfile:
-            Remove_last = line[:-1]
-            list.append(Remove_last)
-    return list
-if os.path.exists('output/video.srt'):
-    os.remove("output/video.srt")
+    base, ext = os.path.splitext(out_file)
+    print(base)
+    print(ext)
+    new_file = video_id + '.mp3'
+    os.rename(out_file, youtube_audio_path+new_file)
 
 
+def get_srt_from_audio(audio_paths: str, video_id: str):
+    path_fileName = audio_paths + video_id + '.mp3'
+    command = f'python3 autosub/main.py --model {model_file_path} --scorer {lm_file_path} --file {path_fileName} --format srt'
+    os.system(command)
+
+    output_path = './output/'+video_id+'.srt'
+    lines = list()
+
+    # Now read the file
+    with open(output_path) as f:
+        lines = f.read()
+
+    return lines
+
+def get_srt_from_file(path: str, fileName: str):
+    path_fileName = f'{path}{fileName}'
+    command = f'python3 autosub/main.py --model {model_file_path} --scorer {lm_file_path} --file {path_fileName} --format srt'
+    os.system(command)
+
+    file_name = fileName[:-4]
+    output_path = './output/'+file_name+'.srt'
+    lines = list()
+
+    # Now read the file
+    with open(output_path) as f:
+        lines = f.read().splitlines()
+
+    return lines
 
 
-def convertTime(seconds):
-    return time.strftime("%H:%M:%S",time.gmtime(seconds))
+def process(video_id:str):
 
-@app.exception_handler(UnicornException)
-async def unicorn_exception_handler(request: Request, exc: UnicornException):
-    return JSONResponse(
-        status_code=418,
-        content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
-    )
+    lines = list()
 
-@app.get("/srtgenerator/{video_id}")
-async def srt_gen(video_id):   
     try:
         sub = YouTubeTranscriptApi.get_transcript(video_id)
         print(f"subtitles are available for {video_id}")
+
         sub_frames = []
         for i, s in enumerate(sub):
-            
             sub_frames.append(srt.Subtitle(
-                index=i+1,
+                index=i + 1,
                 start=datetime.timedelta(s['start']),
-                end=datetime.timedelta(s['start']+s['duration']),
+                end=datetime.timedelta(s['start'] + s['duration']),
                 content=s['text']
             ))
+        lines = srt.compose(sub_frames)
 
-        sub_srt = srt.compose(sub_frames)
-
+        # output_path = output_base_path+video_id
+        # with open(output_path, "w") as sub_file:
+        #     sub_file.write(sub_srt)
 
     except TranscriptsDisabled:
-        raise HTTPException(status_code=404, detail="Opps, seems like video in English language based, sorry at this moment we only convert English audio at this time")
-    return sub_srt
+        print(f"subtitles are missing for {video_id}")
+        download_audio(video_id)
+        print("Downloaded audio for the given video ID.")
+        lines = get_srt_from_audio(audio_paths, video_id)
+        print("Download the SRT too.")
+
+    return lines
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+@app.get("/srtgenerator/{video_id}")
+async def srt_gen(video_id):
+    return process(video_id)
+
+@app.post("/externalfile/", tags=['Currently we support .mp3 and .mp4 files with english audio language.'])
+async def external_file(file: UploadFile = File(...)):
+    with open(f'{audio_paths}{file.filename}', "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return get_srt_from_file(audio_paths, file.filename)
